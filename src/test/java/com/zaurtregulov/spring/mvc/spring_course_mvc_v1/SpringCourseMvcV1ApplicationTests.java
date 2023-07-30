491964a9041aabef204eac96cdc455bea28b0705
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-30 16:45:22 +0300

    spring_course_mvc_v1

--- src/test/java/com/zaurtregulov/spring/mvc/spring_course_mvc_v1/SpringCourseMvcV1ApplicationTests.java
+++ src/test/java/com/zaurtregulov/spring/mvc/spring_course_mvc_v1/SpringCourseMvcV1ApplicationTests.java
@@ -1,13 +1,13 @@
 package com.zaurtregulov.spring.mvc.spring_course_mvc_v1;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringCourseMvcV1ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringCourseMvcV1ApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
---
+++ webapp/WEB-INF/view/show-emp-detail-view.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+
+<body>
+<h2>Dear Employee, you are Welcome!!!</h2>
+<br>
+<br>
+<br>
+
+<%--Your name: ${param.employeeName}--%>
+Your name: ${nameAttribute}
+
+</body>
+
+</html>

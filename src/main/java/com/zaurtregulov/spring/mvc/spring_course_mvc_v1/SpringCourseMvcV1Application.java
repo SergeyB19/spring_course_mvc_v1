8cf04f76715879b2b5f13e4b2ada472865b9ec8e
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-07-30 16:16:29 +0300

    Initial commit

---
+++ src/main/java/com/zaurtregulov/spring/mvc/spring_course_mvc_v1/SpringCourseMvcV1Application.java
@@ -0,0 +1,13 @@
+package com.zaurtregulov.spring.mvc.spring_course_mvc_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseMvcV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCourseMvcV1Application.class, args);
+    }
+
+}
